0dba6683181dd4ddcb1d48e540f93b0c49a142b8
Kippiii <ianorzel@gmail.com>
2020-10-02 21:14:23 -0400

    Updating the algorithm

--- Project 16/diet.py
+++ Project 16/diet.py
@@ -9,14 +9,21 @@ weights = [int(line) for line in stdin]
 
 weights.sort(reverse=True)
 midpoint = sum(weights) // 2  # Saves the max value of the solution
-first_day = []  # Stores the values to be printed
-cur_sum = 0  # Stores the current sum of values
 
-# Greedily takes largest bunch that keeps it within range
-for w in weights:
-    if cur_sum + w <= midpoint:
-        cur_sum += w
-        first_day.append(w)
+best_sum = 0  # Stores the highest sum that can be reached
+best_sol = []  # Stores the best solution that can be found
+# Greedily takes largest bunch that keeps it within range for each start
+for i in range(len(weights)):
+    cur_sum = 0  # Stores the current sum of values
+    cur_sol = []  # Stores the current solution
+    for j in range(i, len(weights)):
+        w = weights[j]  # Stores the current weight
+        if cur_sum + w <= midpoint:
+            cur_sum += w
+            cur_sol.append(w)
+    if cur_sum > best_sum:  # Runs if new solution is better
+        best_sum = cur_sum
+        best_sol = cur_sol
 
-stdout.write(" ".join(str(i) for i in first_day[::-1]))
-stdout.write("\n")
+for i in best_sol[::-1]:
+    stdout.write(f'{i}\n')
