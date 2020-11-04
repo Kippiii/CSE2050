6f35ea94e3a3a43504b6b0e288d12b561867976d
Kippiii <ianorzel@gmail.com>
2020-11-04 16:39:55 -0500

    Fixing some bugs in the program

--- Project 19/poachers.py
+++ Project 19/poachers.py
@@ -4,14 +4,18 @@
 """Calculate the safety of manatees based on their positions."""
 
 from sys import stdin, stdout
-from scipy.spatial import ConvexHull
+from scipy.spatial import ConvexHull, qhull
 
 
 def is_in_hull(hull, point):
     """Determine whether point is within convex hull."""
-    for i in range(len(hull.vertices)-1):
+    if hull is None:
+        return False
+
+    for i in range(len(hull.vertices)):
+        j = i + 1 if i != len(hull.vertices) - 1 else 0
         a = hull.points[hull.vertices[i]]
-        b = hull.points[hull.vertices[i+1]]
+        b = hull.points[hull.vertices[j]]
         dot_product = ((a[1] - b[1]) * (point[0] - a[0])) \
             + ((b[0] - a[0]) * (point[1] - a[1]))
         if dot_product < 0:
@@ -26,8 +30,14 @@ poachers = [list(map(int, stdin.readline().split())) for _ in range(p)]
 manatees = [list(map(int, stdin.readline().split())) for _ in range(m)]
 
 # Generates the two convex hulls needed for the program
-warden_hull = ConvexHull(wardens)
-poacher_hull = ConvexHull(poachers)
+try:
+    warden_hull = ConvexHull(wardens)
+except qhull.QhullError:
+    warden_hull = None
+try:
+    poacher_hull = ConvexHull(poachers)
+except qhull.QhullError:
+    poacher_hull = None
 
 # Loops through each manatee that was input
 for m in manatees:
