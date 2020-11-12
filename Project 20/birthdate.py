004b7c0f9dfd90f7702b3be08efe99ff2d433c6c
Kippiii <ianorzel@gmail.com>
2020-11-12 10:55:20 -0500

    Fixing the range on the sliders

--- Project 20/birthdate.py
+++ Project 20/birthdate.py
@@ -25,7 +25,7 @@ def read_database():
 
     # Stores information from database in table
     freq_table = [[[0 for _ in range(12)] for _ in range(31)]
-                  for _ in range(121)]
+                  for _ in range(131)]
     while True:
         (tup) = cursor.fetchone()
         if not tup:
@@ -34,7 +34,7 @@ def read_database():
         year = int(date[:4])
         month = int(date[5:7])
         day = int(date[8:10])
-        if 1880 <= year <= 2000:
+        if 1880 <= year <= 2010:
             freq_table[year-1880][day-1][month-1] += count
 
     return freq_table
@@ -47,14 +47,14 @@ def get_freq_table(start, end):
     """Get a frequency table independent of year based on range."""
     freq_table = [[0 for _ in range(12)] for _ in range(31)]
     # Loops through every date in the range
-    for y in range(start-1880, end-1880+1):
+    for y in range(start-1880, end-1880):
         for d in range(31):
             for m in range(12):
                 freq_table[d][m] += freq_table_with_year[y][d][m]
     return freq_table
 
 
-freq_table = get_freq_table(1880, 2000)
+freq_table = get_freq_table(1880, 1890)
 
 # Creates the heatmap with axes and lables
 fig, ax = matplotlib.pyplot.subplots(figsize=(10, 15))
@@ -69,15 +69,15 @@ start_axis = matplotlib.pyplot.axes((0.25, 0.05, 0.65, 0.03))
 year_axis = matplotlib.pyplot.axes((0.25, 0.02, 0.65, 0.03))
 start_slider = matplotlib.widgets.Slider(start_axis, "Start", 1880, 2000,
                                          valinit=1880, valstep=1)
-year_slider = matplotlib.widgets.Slider(year_axis, "Years", 0, 120,
-                                        valinit=120, valstep=1)
+year_slider = matplotlib.widgets.Slider(year_axis, "Years", 1, 10,
+                                        valinit=10, valstep=1)
 
 
 def update_graph(*args):
     """Update the heatmap based on slider state."""
     start_year = start_slider.val
     year_count = year_slider.val
-    freq_table = get_freq_table(start_year, min(start_year + year_count, 2000))
+    freq_table = get_freq_table(start_year, start_year + year_count)
     ax.imshow(freq_table, cmap="YlOrRd")
 
 
